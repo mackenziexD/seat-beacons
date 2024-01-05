9f9aac1050e36f6a61f3bdceb63f812421d18d73
Mackenzie James <mackenzie.14@hotmail.co.uk>
2024-01-05 15:40:05 +0000

    trying to fix notifications

--- src/Console/CheckBeaconFuel.php
+++ src/Console/CheckBeaconFuel.php
@@ -9,7 +9,7 @@ use Carbon\Carbon;
 use Seat\Notifications\Models\NotificationGroup;
 use Illuminate\Support\Facades\Notification;
 use Helious\SeatBeacons\Notifications\StuctureWarnings;
-
+use Seat\Notifications\Traits\NotificationDispatchTool;
 use Seat\Web\Models\User;
 
 
@@ -20,6 +20,8 @@ use Seat\Web\Models\User;
  */
 class CheckBeaconFuel extends Command
 {
+    use NotificationDispatchTool;
+    
     /**
      * @var string
      */
@@ -55,61 +57,20 @@ class CheckBeaconFuel extends Command
         // dont send empty message
         if($structureMessage === '') return;
 
-        // detect handlers setup for the current notification
-        $handlers = config('notifications.alerts.seat_beacons_warnings.handlers', []);
-
-        // retrieve routing candidates for the current notification
-        $routes = $this->getRoutingCandidates();
-
-        // in case no routing candidates has been delivered, exit
-        if ($routes->isEmpty())
-            return;
-
-        // attempt to enqueue a notification for each routing candidates
-        $routes->each(function ($integration) use ($handlers, $structureMessage) {
-            if (array_key_exists($integration->channel, $handlers)) {
-
-                // extract handler from the list
-                $handler = $handlers[$integration->channel];
-
-                // enqueue the notification
-                Notification::route($integration->channel, $integration->route)
-                    ->notify(new $handler($structureMessage));
-            }
-        });
-
-    }
-
-    /**
-     * Provide a unique list of notification channels (including driver and route).
-     *
-     * @return \Illuminate\Support\Collection
-     */
-    private function getRoutingCandidates()
-    {
-        $settings = NotificationGroup::with('alerts')
+        // Get notification groups with 'seat_beacons_warnings' alert type
+        $groups = NotificationGroup::with('alerts')
             ->whereHas('alerts', function ($query) {
                 $query->where('alert', 'seat_beacons_warnings');
             })->get();
 
-        $routes = $settings->map(function ($group) {
-            return $group->integrations->map(function ($channel) {
-
-                // extract the route value from settings field
-                $settings = (array) $channel->settings;
-                $key = array_key_first($settings);
-                $route = $settings[$key];
-
-                // build a composite object built with channel and route
-                return (object) [
-                    'channel' => $channel->type,
-                    'route' => $route,
-                ];
+        // Check if there are any notification groups
+        if (!$groups->isEmpty()) {
+            // Dispatch notifications using the dispatchNotifications method
+            $this->dispatchNotifications('seat_beacons_warnings', $groups, function ($notificationClass) use ($structureMessage) {
+                return new $notificationClass($structureMessage);
             });
-        });
+        }
 
-        return $routes->flatten()->unique(function ($integration) {
-            return $integration->channel . $integration->route;
-        });
+
     }
 }
--- src/Notifications/StuctureWarnings.php
+++ src/Notifications/StuctureWarnings.php
@@ -3,44 +3,32 @@
 namespace Helious\SeatBeacons\Notifications;
 
 use Seat\Notifications\Notifications\AbstractDiscordNotification;
-use Seat\Notifications\Notifications\AbstractNotification;
-use Illuminate\Notifications\Messages\SlackMessage;
-use Illuminate\Queue\SerializesModels;
-use Illuminate\Contracts\Queue\ShouldQueue;
 use Seat\Notifications\Services\Discord\Messages\DiscordEmbed;
 use Seat\Notifications\Services\Discord\Messages\DiscordEmbedField;
 use Seat\Notifications\Services\Discord\Messages\DiscordMessage;
 
-class StuctureWarnings extends AbstractDiscordNotification implements ShouldQueue
-{
-    use SerializesModels;
-
+/**
+ * Class StuctureWarnings.
+ *
+ * @package Seat\Kassie\Calendar\Notifications
+ */
+class StuctureWarnings extends AbstractDiscordNotification
+{   
     private $message;
-
+    
     public function __construct($message)
     {
         $this->message = $message;
     }
 
-    /**
-     * @param DiscordMessage $message
-     * @param $notifiable
-     * @return void
+ /**
+     * @param  DiscordMessage  $message
+     * @param  $notifiable
      */
-    protected function populateMessage(DiscordMessage $message, $notifiable)
-    {
-        $message->from('SeAT Beacons');
-
-        // handle max character count for discord
-        if (strlen($this->message) > 2000) {
-            $content = 'Message too long to send to Discord. Please check the SeAT Beacons page for more information.';
-        } else {
-            $content = PHP_EOL . $this->message;
-        }
-
-        $message->content($content);
-
-        return $message;
-
+    public function populateMessage(DiscordMessage $message, $notifiable)
+    {        
+        $message
+            ->from('SeAT Beacons')
+            ->content('**Structure Data**' . PHP_EOL . $this->message);
     }
 }
